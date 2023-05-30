676baa934e629c7ac187285b03ca60c7f8e08dde
cuimeng <38125791+cccccccccm@users.noreply.github.com>
2023-05-30 20:39:36 +0800

    Update monocle_pseudotime_analysis.r

--- monocle_pseudotime_analysis.r
+++ monocle_pseudotime_analysis.r
@@ -7,7 +7,7 @@ library(RColorBrewer)
 library(DDRTree)
 library(ggplot2)
 
-##读取数据
+##reading data from Seurat object
 data <- as(as.matrix(seurat@assays$RNA@counts), 'sparseMatrix')
 pd <- new('AnnotatedDataFrame', data = seurat@meta.data)
 fData <- data.frame(gene_short_name = row.names(data), row.names = row.names(data))
@@ -20,17 +20,14 @@ monocle_cds <- newCellDataSet(data,
          
 monocle_cds <- estimateSizeFactors(monocle_cds)
 monocle_cds <- estimateDispersions(monocle_cds)         
-# 过滤低于1%细胞中检出的基因，最低表达阈值为0.1
+# Filtering of genes detected in less than 1% of cells with a minimum expression threshold of 0.1
 monocle_cds <- detectGenes(monocle_cds, min_expr = 0.1)
 expressed_genes <- row.names(subset(Biobase::fData(monocle_cds), num_cells_expressed > nrow(pd) * 0.05))
 
-
-# 查看筛选后的基因个数（用于后面基因的筛选）
-# 对细胞进行过滤
 HSMM <- monocle_cds[expressed_genes,]
 
 
-#Trajectory step 1 关键基因的筛选
+#Trajectory step 1 Screening for genes for differentiation analysis
 
 disp_table <- dispersionTable(HSMM)
 disp.genes <- subset(disp_table, mean_expression >= 0.1 & dispersion_empirical >= 1 * dispersion_fit)$gene_id
@@ -45,15 +42,13 @@ dir <- paste0("./HBV/monocle2/CD8/hbvCTL2/")
 if(!file.exists(dir)){
 	dir.create(dir,recursive = TRUE)
 }
-setwd(dir)
-
+###############saving pictures
+file <- paste0(dir,"/","group.pdf")
 p <- plot_cell_trajectory(HSMM, color_by = "group_name",cell_size = 0.5)+ 
 	scale_color_manual(values = c('#1BB4B8','#FF6666','#A07DB6','#79A72C','#999999'))+
 	theme(legend.title = element_blank())+
 	guides(color = guide_legend(override.aes = list(size = 2)))
-file <- paste0(dir,"/","group.pdf").
 ggsave(file,p,width =5,height = 4)
-
 
 
 p <- plot_cell_trajectory(HSMM, show_cell_names = F, color_by = "Pseudotime") + scale_color_viridis_c()
