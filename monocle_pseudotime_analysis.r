d3f683c1e7bf5da8cd53193a6d92489e633dbf37
cuimeng <38125791+cccccccccm@users.noreply.github.com>
2023-07-04 10:37:16 +0800

    Update monocle_pseudotime_analysis.r

--- monocle_pseudotime_analysis.r
+++ monocle_pseudotime_analysis.r
@@ -7,7 +7,7 @@ library(RColorBrewer)
 library(DDRTree)
 library(ggplot2)
 
-##reading data from Seurat object
+#reading data from Seurat object
 data <- as(as.matrix(seurat@assays$RNA@counts), 'sparseMatrix')
 pd <- new('AnnotatedDataFrame', data = seurat@meta.data)
 fData <- data.frame(gene_short_name = row.names(data), row.names = row.names(data))
@@ -23,17 +23,13 @@ monocle_cds <- estimateDispersions(monocle_cds)
 # Filtering of genes detected in less than 1% of cells with a minimum expression threshold of 0.1
 monocle_cds <- detectGenes(monocle_cds, min_expr = 0.1)
 expressed_genes <- row.names(subset(Biobase::fData(monocle_cds), num_cells_expressed > nrow(pd) * 0.05))
-
 HSMM <- monocle_cds[expressed_genes,]
 
 
 #Trajectory step 1 Screening for genes for differentiation analysis
-
 disp_table <- dispersionTable(HSMM)
 disp.genes <- subset(disp_table, mean_expression >= 0.1 & dispersion_empirical >= 1 * dispersion_fit)$gene_id
 HSMM <- setOrderingFilter(HSMM, disp.genes)
-
-
 #Trajectory step 2: reduce data dimensionality  
 HSMM <- reduceDimension(HSMM, max_components = 2,method = 'DDRTree')
 #Trajectory step 3: order cells along the trajectory  
